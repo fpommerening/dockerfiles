3910e5df67890650231ffcffcc162910116377e6
Frank Pommerening <frank@pommerening-online.de>
2017-10-10 19:10:26 +0000

    update mono to version 5.2.0.224

--- rpi/Mono/build.sh
+++ rpi/Mono/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker build -t fpommerening/rpi-mono:wheezy .
-docker build -t fpommerening/rpi-mono:wheezy-4 .
-docker build -t fpommerening/rpi-mono:wheezy-4.8 .
-docker build -t fpommerening/rpi-mono:wheezy-4.8.0 .
-docker build -t fpommerening/rpi-mono:wheezy-4.8.0.495 .
+docker build -t fpommerening/rpi-mono:jessie .
+docker build -t fpommerening/rpi-mono:jessie-5 .
+docker build -t fpommerening/rpi-mono:jessie-5.2 .
+docker build -t fpommerening/rpi-mono:wheezy-5.2.0 .
+docker build -t fpommerening/rpi-mono:wheezy-5.2.0.224 .
